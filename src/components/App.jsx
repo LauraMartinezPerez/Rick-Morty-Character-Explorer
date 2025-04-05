db7375f30b4773c3ad20a316abb129fa5b51c7cd
Laura <laura.laukiz@gmail.com>
2025-04-05 22:30:22 +0200

    bonus

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -46,8 +46,7 @@ const changeSpecies = (valueSpecies) => {
     }).filter((character) => {
         return filterSpecies === "" ? true : filterSpecies === character.species;
     }).sort((a, b) => {
-      return a.name.localeCompare(b.name); //localeCompare ordena cadenas y respeta idioma/mayus/minus
-  });
+      return a.name.localeCompare(b.name); //BONUS 8: sort ordena alfabeticamente y localeCompare maneja las mayúsculas y minúsculas según el idioma
     
 
     const { pathname } = useLocation();
