e8ae9b9fc5b9a399897f795382058ee3e7420080
Laura <laura.laukiz@gmail.com>
2025-04-06 23:58:47 +0200

    ajustando estilos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
-
+import logo from '../images/rick-morty-logo.webp';
 
 function Header() {
   return (
     <header className="header">
+      <img className="header__img" src={logo} alt="" />
 
        
     </header>
--- src/components/NoCharacter.jsx
+++ src/components/NoCharacter.jsx
@@ -4,7 +4,7 @@ function NoCharacter({filterName}) {
   return (
     <div className="NoCharacter">
         <p className="NoCharacter__text">No hay ningún personaje que coincida con el nombre "{filterName}"</p>
-        <img className="NoCharacter__img"src={sorpresaImage} alt="" />
+        <img className="NoCharacter__img"src={sorpresaImage} alt="rick y morty sorprendidos" />
     </div>
   )
 }
